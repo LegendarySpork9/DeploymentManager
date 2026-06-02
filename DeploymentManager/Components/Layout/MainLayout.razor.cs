e284b53f173bf10c88b37fbade3a2eff5a4122d0
LegendarySpork9 <100041525+LegendarySpork9@users.noreply.github.com>
2026-06-02 21:13:41 +0100

    5/login page (#21)
    * Login Page
    
    Created the login page for the application.
    
    * Unit Tests
    
    Changed the HashFunction to return the value if null or whitespace.
    
    Added unit tests.
    
    * Login Fix
    
    Fixed the capturing of details from the form.

---
+++ DeploymentManager/Components/Layout/MainLayout.razor.cs
@@ -0,0 +1,66 @@
+﻿// Copyright © - Unpublished - Toby Hunter
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Server.ProtectedBrowserStorage;
+
+namespace DeploymentManager.Components.Layout
+{
+    public partial class MainLayout
+    {
+        [Inject]
+        private NavigationManager Navigation { get; set; } = default!;
+        [Inject]
+        private ProtectedSessionStorage SessionStorage { get; set; } = default!;
+
+        private bool IsLoggedIn;
+        private bool IsInitialised;
+
+        /// <summary>
+        /// Checks if the user is logged in.
+        /// </summary>
+        protected override async Task OnAfterRenderAsync(bool firstRender)
+        {
+            if (firstRender)
+            {
+                try
+                {
+                    ProtectedBrowserStorageResult<bool> loggedInResult = await SessionStorage.GetAsync<bool>("userLoggedIn");
+
+                    if (loggedInResult.Success && (loggedInResult.Value))
+                    {
+                        IsInitialised = true;
+                        IsLoggedIn = true;
+                        StateHasChanged();
+                    }
+
+                    else
+                    {
+                        Navigation.NavigateTo(
+                            "/login",
+                            forceLoad: true);
+                    }
+                }
+
+                catch
+                {
+                    Navigation.NavigateTo(
+                        "/login",
+                        forceLoad: true);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Performs the logout steps.
+        /// </summary>
+        private async Task SignOut()
+        {
+            await SessionStorage.DeleteAsync("userLoggedIn");
+
+            IsLoggedIn = false;
+
+            Navigation.NavigateTo(
+                "/login",
+                forceLoad: true);
+        }
+    }
+}
---
+++ DeploymentManager/Functions/Hash Function.cs
@@ -0,0 +1,33 @@
+﻿// Copyright © - Unpublished - Toby Hunter
+using System.Security.Cryptography;
+using System.Text;
+
+namespace DeploymentManager.Functions
+{
+    public static class HashFunction
+    {
+        /// <summary>
+        /// Takes the given value and hashes it.
+        /// </summary>
+        public static string HashString(string value)
+        {
+            string hashString = value;
+
+            if (!string.IsNullOrWhiteSpace(value))
+            {
+                StringBuilder hashedValue = new();
+
+                byte[] hashBytes = SHA512.HashData(Encoding.UTF8.GetBytes(value));
+
+                for (int i = 0; i < hashBytes.Length; i++)
+                {
+                    hashedValue.Append(hashBytes[i].ToString("x2"));
+                }
+
+                hashString = hashedValue.ToString();
+            }
+
+            return hashString;
+        }
+    }
+}
